159892def489da4625f7402520b3246dcc6ea09c
Cristian Mundo <37192145+AzzarCM@users.noreply.github.com>
2019-11-20 16:44:18 -0600

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,27 @@
+//10. Hacer una función en C++ que, dado un BST y un dato, encuentre el sucesor POST ORDER del
+nodo que contiene dicho dato.
+
+Node* postorderPredecessor(Node* root, Node* n) 
+{ 
+    // If right child exists, then it is postorder 
+    // predecessor. 
+    if (n->right) 
+        return n->right; 
+  
+    // If right child does not exist, then 
+    // travel up (using parent pointers) 
+    // until we reach a node which is right 
+    // child of its parent. 
+    Node *curr = n, *parent = curr->parent; 
+    while (parent != NULL && parent->left == curr) { 
+        curr = curr->parent; 
+        parent = parent->parent; 
+    } 
+  
+    // If we reached root, then the given 
+    // node has no postorder predecessor 
+    if (parent == NULL) 
+        return NULL; 
+  
+    return parent->left; 
+} 
