dbb3593098f9c312a3efa03644009491952c0ea2
David Vallecios <37277365+DavidVallecios@users.noreply.github.com>
2019-11-20 12:47:09 -0600

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+//. Hacer una función en C++ que, dado un árbol T, determine si es un BST válido
+int isBST(node* node)  
+{  
+    return(isBSTUtil(node, INT_MIN, INT_MAX));  
+}  
+  
+
+int isBSTUtil(node* node, int min, int max)  
+{  
+
+    if (node==NULL)  
+        return 1;  
+              
+    if (node->data < min || node->data > max)  
+        return 0;  
+    return
+        isBSTUtil(node->left, min, node->data-1) &&
+        isBSTUtil(node->right, node->data+1, max); 
+}
