32056d4a2fbb342fb3e8bab6eb1db14c6f0def6b
Cristian Mundo <37192145+AzzarCM@users.noreply.github.com>
2019-11-20 15:24:55 -0600

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,30 @@
+// Modificar el ejercicio anterior, para que el BST resultante sea lo más balanceado posible
+
+void storeBSTNodes(Node* root, vector<Node*> &nodes) 
+{ 
+    if (root==NULL) 
+        return; 
+    storeBSTNodes(root->left, nodes); 
+    nodes.push_back(root); 
+    storeBSTNodes(root->right, nodes); 
+} 
+
+Node* buildTreeUtil(vector<Node*> &nodes, int start, 
+                   int end) 
+{ 
+    if (start > end) 
+        return NULL; 
+    int mid = (start + end)/2; 
+    Node *root = nodes[mid]; 
+    root->left  = buildTreeUtil(nodes, start, mid-1); 
+    root->right = buildTreeUtil(nodes, mid+1, end); 
+    return root; 
+} 
+  
+Node* buildTree(Node* root) 
+{ 
+    vector<Node *> nodes; 
+    storeBSTNodes(root, nodes); 
+    int n = nodes.size(); 
+    return buildTreeUtil(nodes, 0, n-1); 
+} 
