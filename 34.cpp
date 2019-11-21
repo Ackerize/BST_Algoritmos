e89420794bf23429ec863f6521cd467bad14f93e
David Vallecios <37277365+DavidVallecios@users.noreply.github.com>
2019-11-20 19:07:29 -0600

    Create 34.cpp

---
+++ 34.cpp
@@ -0,0 +1,11 @@
+//. Hacer una función en C++ que, dado un BST y un entero N, devuelva una lista enlazada circular
+// con todos los datos menores o iguales a N que estén en el BST.
+
+LC inorderTraversal(struct node *root, int N, LC lista){
+    if (root != NULL){
+        inorder(root->left, N, lista);
+        if(root->key <= N)
+            lista.agregarElemento(root->key);
+        inorder(root->right, N, lista);
+    }
+}
