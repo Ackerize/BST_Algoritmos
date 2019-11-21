22a619e0d46b8de9df6d057e7a84edda004082da
David Vallecios <37277365+DavidVallecios@users.noreply.github.com>
2019-11-20 19:11:17 -0600

    Update 15.java

--- 15.java
+++ 15.java
@@ -1,38 +1,29 @@
-   boolean isPairPresent(Node node, int target) 
-    { 
-        // This list a1 is passed as an argument 
-        // in treeToList method 
-        // which is later on filled by the values of BST 
-        ArrayList<Integer> a1 = new ArrayList<>(); 
-  
-        // a2 list contains all the values of BST 
-        // returned by treeToList method 
-        ArrayList<Integer> a2 = treeToList(node, a1); 
-  
-        int start = 0; // Starting index of a2 
-  
-        int end = a2.size() - 1; // Ending index of a2 
-  
-        while (start < end) { 
-  
-            if (a2.get(start) + a2.get(end) == target) // Target Found! 
-            { 
-                System.out.println("Pair Found: " + a2.get(start) + " + " + a2.get(end) + " "
-                                   + "= " + target); 
-                return true; 
-            } 
-  
-            if (a2.get(start) + a2.get(end) > target) // decrements end 
-            { 
-                end--; 
-            } 
-  
-            if (a2.get(start) + a2.get(end) < target) // increments start 
-            { 
-                start++; 
-            } 
-        } 
-  
-        System.out.println("No such values are found!"); 
+ // . Hacer una función en C++ que, dado un BST y un entero N, encuentre dos nodos en el BST
+// cuyos datos sumen N.
+
+bool findpairUtil(Node* root, int sum,  unordered_set<int> &set) 
+{ 
+    if (root == NULL) 
         return false; 
+  
+    if (findpairUtil(root->left, sum, set)) 
+        return true; 
+  
+    if (set.find(sum - root->data) != set.end()) { 
+        cout << "Pair is found ("
+             << sum - root->data << ", "
+             << root->data << ")" << endl; 
+        return true; 
     } 
+    else
+        set.insert(root->data); 
+  
+    return findpairUtil(root->right, sum, set); 
+} 
+  
+void findPair(Node* root, int sum) 
+{ 
+    unordered_set<int> set; 
+    if (!findpairUtil(root, sum, set)) 
+        cout << "Pairs do not exit" << endl; 
+} 
